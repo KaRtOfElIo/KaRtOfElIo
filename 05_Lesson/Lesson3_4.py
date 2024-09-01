4a82e574ce58f1a7f7708648c1469a37325c4df3
KaRtOfElIo <164926717+KaRtOfElIo@users.noreply.github.com>
2024-09-01 13:34:39 +0300

    Update Lesson3_4.py

--- 05_Lesson/Lesson3_4.py
+++ 05_Lesson/Lesson3_4.py
@@ -3,11 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox(service=service)
 
 try:
-    
     driver.get("http://the-internet.herokuapp.com/entry_ad")
     wait = WebDriverWait(driver, 10)
     modal_window = wait.until(
@@ -16,6 +16,7 @@ try:
     close_button = wait.until(EC.element_to_be_clickable(
         (By.CSS_SELECTOR, ".modal-footer")))
     time.sleep(3)
+    # Нажимаем кнопку "Close" в модальном окне
     close_button.click()
     time.sleep(2)
 
