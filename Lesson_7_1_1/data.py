814e2ff5da311d3836c480acad4f71bfe6259565
KaRtOfElIo <bcalkov85@gmail.com>
2024-10-01 21:46:55 +0300

    lesson7

---
+++ Lesson_7_1_1/data.py
@@ -0,0 +1,18 @@
+# Первое задание
+URL_1 = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+first_name = "Иван"
+last_name = "Петров"
+address = "Ленина, 55-3"
+email = "test@skypro.com"
+phone = "+798589999877"
+zip_code = ""
+city = "Москва"
+country = "Россия"
+job_position = "QA"
+company = "SkyPro"
+
+# Второе задание
+URL_2 = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+
+# Третье задание
+URL_3 = "https://www.saucedemo.com/"
