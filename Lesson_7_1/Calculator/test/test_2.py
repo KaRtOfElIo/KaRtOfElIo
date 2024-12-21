02cdb2e1de3d0d12e937118267186bec2b2aaa32
KaRtOfElIo <164926717+KaRtOfElIo@users.noreply.github.com>
2024-12-21 12:50:01 +0300

    Delete Lesson_7_1 directory

--- Lesson_7_1/Calculator/test/test_2.py
+++
@@ -1,7 +0,0 @@
-from Lesson_7.Calculator.Pages.CalcMainPage import CalcMain
-
-def test_calculator_assert(chrome_browser):
-    calcmain = CalcMain(chrome_browser)
-    calcmain.insert_time()
-    calcmain.clicking_buttons()
-    assert "15" in calcmain.wait_button_get_text()
